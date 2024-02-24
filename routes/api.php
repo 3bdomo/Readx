a57ff05c19a55f16b710c868a9a9fae290594389
Abdulrahman Mohamed <abdoo1242002@gmail.com>
2024-02-24 15:24:06 +0200

    logout done

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function(){
 
     Route::post('/register', 'register');
-    
+
     Route::post('/login', 'login');
     Route::post('/logout', 'logout')->middleware('auth:sanctum');
 });
