b9b130b7d84cbce340d72b12ccc9152a1a8f9c07
Abdulrahman Mohamed <abdoo1242002@gmail.com>
2024-02-25 00:46:50 +0200

    delete old tokens line add

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -72,7 +72,8 @@ class AuthController extends Controller
         }
       if(Auth::attempt(['email'=>$request->email, 'password'=>$request->password])) {
           $user = Auth::user();
-         // $request->currentAccessToken()->delete;
+
+          Auth::user()->tokens()->delete();//delete odl tokens
           $data['token'] = $user->createToken('user_token')->plainTextToken;
           $data['name'] = $user->first_name;
           return ApiResponse::SendResponse(200, "login successfully", $data);
