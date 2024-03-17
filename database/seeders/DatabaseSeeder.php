02dcf093e4e1afee00674a3957becba21a928147
Abdulrahman Mohamed <abdoo1242002@gmail.com>
2024-03-17 17:40:50 +0200

    Add Faker library for generating fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,10 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    use HasFactory;
     /**
      * Seed the application's database.
      */
