7d1c162c9cabe8e8a445400bb85fb9c3f6dea9db
Abdulrahman Mohamed <AbdulrahmanMo124@gmail.com>
2024-05-28 02:06:41 +0300

    token expiration

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Models\Admin\Admin;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -28,7 +29,9 @@ class AdminAuthController extends Controller
         if ($admin && Hash::check($request->password, $admin->password))  {
 
           $admin->tokens()->delete();//delete odl tokens
-            $token =  $admin->createToken('MyAuthApp')->plainTextToken;
+            $expiresAt = Carbon::now()->addDays(7);
+            $token = $admin->createToken('MyAuthApp', ['*'], $expiresAt)->plainTextToken;
+
             return ApiResponse::SendResponse(200, 'Login Successful', ['token' => $token]);
         } else {
             return ApiResponse::SendResponse(419, 'Login Failed','Invalid credentials');
